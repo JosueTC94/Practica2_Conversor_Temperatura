891449f5fb9e89de0752909fab964e4e55aad822
Josue Toledo Castro <alu0100763492@ull.edu.es>
2015-10-08 10:23:22 +0100

    Actualizacion archivos e instalacion mocha y cha

--- temperatura.js
+++ temperatura.js
@@ -3,6 +3,7 @@ $(document).ready(function()
 	$("#calcular_temperatura").click(function()
 	{
 		$("#resultado_temperatura").show("slow");
+		calculate();
 	});
 	$("#temperatura").focusin(function()
 	{
@@ -17,3 +18,30 @@ $(document).ready(function()
 			$("#temperatura").css("border-color","grey");
 	});
 });
+
+function calculate() {
+  var result;
+  var original = $("#temperatura").val();
+  var temp = original;
+  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
+
+  var m = temp.match(regexp);
+
+  if (m) {
+    var num = m[1];
+    var type = m[2];
+    num = parseFloat(num);
+    if (type == 'c' || type == 'C') {
+      result = (num * 9/5)+32;
+     	result = result.toFixed(3)+"º F";
+		 }
+    else {
+      result = (num - 32)*5/9;
+      result = result.toFixed(3)+"º C";
+    }
+    $("#resultado_temperatura").html("Resultado->"+result);
+  }
+  else {
+    $("#resultado_temperatura").html("ERROR! Try something like '-4.2C' instead");
+  }
+}
