bfed99ef0a271aafb7de3064e994e7d1d69eee09
Josue Toledo Castro <alu0100763492@ull.edu.es>
2015-10-08 17:24:43 +0100

    Instalacion de gulp y prueba de test

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var minifyHTML = require('gulp-minify-html');
 var minifyCSS  = require('gulp-minify-css');
 
 gulp.task('minify', function () {
-  gulp.src('temperature.js')
+  gulp.src('temperatura.js')
   .pipe(uglify())
   .pipe(gulp.dest('minified'));
 
