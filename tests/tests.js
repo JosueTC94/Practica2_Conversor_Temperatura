184691590b2044494199f92b434db5d24b1b6c15
Josue Toledo Castro <alu0100763492@ull.edu.es>
2015-10-08 19:47:45 +0100

    Modificacion index.html

--- tests/tests.js
+++ tests/tests.js
@@ -1,15 +1,15 @@
 var assert = chai.assert;
 
 suite('temperatura', function() {
-    test('32F = 0C', function() {
+    test('32F = 0.000C', function() {
         original.value = "32F";
         calculate();
-        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+        assert.deepEqual(converted.innerHTML, "0.000º C");
     });
-    test('45C = 113.0 Farenheit', function() {
+    test('45C = 113.000 Farenheit', function() {
         original.value = "45C";
         calculate();
-        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+        assert.deepEqual(converted.innerHTML, "113.000º F");
     });
     test('5X = error', function() {
         original.value = "5X";
